fc6f1335b57f61fcffda2ea59e38c2b6b4f93ad2
Auna Walton <awalton3@nd.edu>
2020-10-07 20:01:44 -0400

    added makefiles and server, client directories

---
+++ client/chatclient.cpp
@@ -0,0 +1,10 @@
+/* Team members: Crystal Colon, Molly Zachlin, Auna Walton 
+ * NetIDs: ccolon2, mzachlin, awalton3
+ * chatclient.cpp - The client side of the TCP chat application  
+ *   
+*/ 
+
+int main() {
+
+	return 0; 
+}
