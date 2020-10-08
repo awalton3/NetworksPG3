9fceb85e0b8854fbbf9d81a2f6d4041437f161c3
Molly Zachlin <mzachlin@nd.edu>
2020-10-07 21:21:45 -0400

    initial structure

--- client/chatclient.cpp
+++ client/chatclient.cpp
@@ -4,7 +4,83 @@
  *   
 */ 
 
-int main() {
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sstream>
+#include <netdb.h>
+#include <sys/time.h>
+#include <unistd.h>
+#define MAX_SIZE 4096
+using namespace std;
+
+void error(int code) {
+    switch (code) {
+        case 1:
+            cout << "Not enough input arguments!" << endl;
+        break; 
+        default:
+            cout << "An unknown error occurred." << endl;
+        break;
+    }
+}
+
+
+int main(int argc, char** argv) {
+    /* Parse command line arguments */
+    if (argc < 4) { // Not enough input args
+        error(1); 
+        return 1;
+    }
+
+    const char* host = argv[1];
+    int port = stoi(argv[2]);
+    char* user = argv[3];
+    struct stat statbuf;
+       
+    /* Translate host name into peer's IP address */
+    struct hostent* hostIP = gethostbyname(host);
+    if (!hostIP) {
+        cout << "Error: Unknown host." << endl;
+        return 1;
+    }
+
+    /* Create address data structure */
+    struct sockaddr_in sock;
+    // Clear memory location
+    bzero((char*) &sock, sizeof(sock));
+    // Specify IPv4
+    sock.sin_family = AF_INET;
+    // Copy host address into sockaddr
+    bcopy(hostIP->h_addr, (char*) &sock.sin_addr, hostIP->h_length);
+    // Convert from host to network byte order
+    sock.sin_port = htons(port);
+
+    /* Create the socket */
+    int sockfd;
+    //socklen_t len = sizeof(sock);
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Error creating socket.");
+        return 1;
+    }
+
+	cout << "Connecting to " << host << " on port " << port << endl; 
+
+    /* Connect */
+    if (connect(sockfd, (struct sockaddr*)&sock, sizeof(sock)) < 0) {
+        perror("Error connecting.");
+        return 1;
+    }
+
+	cout << "Connection established" << endl; 
+    
+    
+
 
 	return 0; 
 }
--- server/chatserver.cpp
+++ server/chatserver.cpp
@@ -1,5 +1,121 @@
-int main() {
+/* Team members: Crystal Colon, Molly Zachlin, Auna Walton 
+ * NetIDs: ccolon2, mzachlin, awalton3
+ * chatserver.cpp - The server side of the TCP chat application  
+ *   
+*/ 
+#include <iostream>
+#include <string>
+
+#include <strings.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fstream>
+#include <time.h>
+#include <stdint.h>
+#include <pthread.h>
+
+#define MAX_SIZE 4096
+#define MAX_PENDING 5
+
+using namespace std;
+
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    int sock = *(int*) socket_desc;
+             
+    return 0;
+} 
+
+void error(int code) {
+    switch (code) {
+        case 1:
+            cout << "Not enough arguments" << endl;
+        break;
+        default:
+            cout << "An unknown error occurred" << endl;
+        break;
+    }
+}
 
 
+int main(int argc, char** argv) {
+
+    /* Parse command line arguments */
+    if (argc < 2) {
+        error(1);
+        return 1;
+    }
+
+    int port = stoi(argv[1]);
+
+    /* Create address data structure */
+    struct sockaddr_in sock;
+    // Clear memory location
+    bzero((char*) &sock, sizeof(sock));
+    // Specify IPv4
+    sock.sin_family = AF_INET;
+    // Use the default IP address of the server
+    sock.sin_addr.s_addr = INADDR_ANY;
+    // Convert from host to network byte order
+    sock.sin_port = htons(port);
+
+    /* Set up socket */
+    int sockfd;
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket failed.");
+        return 1;
+    }
+
+    /* Set socket options */
+    int opt = 1;
+    if ((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,(char*)&opt, sizeof(int))) < 0) {
+        perror("Setting socket options failed.");
+        return 1;
+    }
+
+    /* Bind */ 
+    if ((bind(sockfd, (struct sockaddr*) &sock, sizeof(sock))) < 0) {
+        perror("Bind failed.");
+        return 1;
+    }
+
+    /* Listen */
+    if ((listen(sockfd, MAX_PENDING)) < 0) {
+        perror("Listen failed.");
+        return 1; 
+    } 
+
+    /* Wait for incoming connections */
+    int new_sockfd;
+    struct sockaddr_in client_sock;
+    socklen_t len = sizeof(client_sock);
+    pthread_t thread_id;
+
+    while (1) {
+
+        cout << "Waiting for connections on port " << port << endl;
+        if ((new_sockfd = accept(sockfd, (struct sockaddr*) &client_sock, &len)) < 0) {
+			cout << "exiting " << endl; 
+            perror("Accept failed.");
+			cout << "exiting " << endl; 
+            return 1;
+        }
+
+        cout << "Connection established." << endl;
+
+        if(pthread_create(&thread_id, NULL, connection_handler, (void*) &client_sock) < 0) {
+            perror("could not create thread");
+            return 1;
+        }
+ 
+    }
 	return 0;
 }
